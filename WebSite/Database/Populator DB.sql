86eb52352385a25cebc5f2eb0ed9c6cd750e5252
thatsfaso <i.fasolino3@studenti.unisa.it>
2023-12-25 20:27:57 +0100

    Homepage frontend stuff + DB upload nuovo + SearchBar files upload (todo)

---
+++ WebSite/Database/Populator DB.sql
@@ -0,0 +1,173 @@
+-- Popolamento della tabella Cliente
+INSERT INTO Cliente (email, nome, cognome, tipo_utente, data_nascita, numero_carta, data_scadenza, verificato, password) VALUES
+('cliente1@example.com', 'Mario', 'Rossi', 'Studente', '1990-01-15', '1234567812345670', '2025-01-01', true, 'password123'),
+('cliente2@example.com', 'Luigi', 'Verdi', 'Locatore', '1985-05-20', '9876543298765432', '2024-12-01', true, 'securepass'),
+('cliente3@example.com', 'Anna', 'Bianchi', 'Studente', '1998-03-10', '5678901234567890', '2023-11-01', true, 'annapass'),
+('cliente4@example.com', 'Giulia', 'Neri', 'Locatore', '1992-08-28', '6543210987654321', '2025-02-01', true, 'giuliapassword'),
+('cliente5@example.com', 'Alessio', 'Giallo', 'Studente', '2000-12-05', NULL, NULL, true, 'alessio123'),
+('cliente6@example.com', 'Francesca', 'Verde', 'Studente', '1995-07-22', '8765432109876543', '2023-10-01', true, 'francescapass'),
+('cliente7@example.com', 'Davide', 'Blu', 'Locatore', '1989-04-18', '3456789012345678', '2024-09-01', true, 'davidepass'),
+('cliente8@example.com', 'Elena', 'Rosa', 'Studente', '1996-11-30', '2345678901234567', '2023-08-01', true, 'elenapassword'),
+('cliente9@example.com', 'Matteo', 'Arancio', 'Studente', '2003-02-14', NULL, NULL, true, 'matteo2022'),
+('cliente10@example.com', 'Giorgio', 'Viola', 'Locatore', '1994-09-08', '4321098765432109', '2024-06-01', true, 'giorgiopass');
+
+-- Popolamento della tabella Dipendente
+INSERT INTO Dipendente (email, nome, cognome, tipo_dipendente, password) VALUES
+('dipendente1@example.com', 'Giovanna', 'Bianchi', 'Homechecker', 'dipendente123'),
+('admin@example.com', 'Admin', 'Admin', 'Admin', 'adminpass'),
+('dipendente2@example.com', 'Luca', 'Verdi', 'Homechecker', 'homecheckluca');
+
+-- Popolamento della tabella Post
+INSERT INTO Post (titolo, descrizione, email) VALUES
+('Titolo Post 1', 'Descrizione Post 1', 'cliente1@example.com'),
+('Titolo Post 2', 'Descrizione Post 2', 'cliente2@example.com'),
+('Titolo Post 3', 'Descrizione Post 3', 'cliente3@example.com'),
+('Titolo Post 4', 'Descrizione Post 4', 'cliente4@example.com'),
+('Titolo Post 5', 'Descrizione Post 5', 'cliente5@example.com');
+
+-- Popolamento della tabella Passione
+INSERT INTO Passione (nome) VALUES
+('Viaggi'),
+('Sport'),
+('Cucina'),
+('Musica'),
+('Arte');
+
+-- Popolamento della tabella Assegnazione
+INSERT INTO Assegnazione (id_passione, email) VALUES
+(1, 'cliente1@example.com'),
+(2, 'cliente1@example.com'),
+(2, 'cliente2@example.com'),
+(3, 'cliente3@example.com'),
+(4, 'cliente4@example.com'),
+(5, 'cliente5@example.com'),
+(1, 'cliente6@example.com'),
+(2, 'cliente6@example.com'),
+(3, 'cliente7@example.com'),
+(4, 'cliente8@example.com');
+
+-- Popolamento della tabella Segnalazione
+INSERT INTO Segnalazione (email, emailS, stato, motivo) VALUES
+('cliente1@example.com', 'cliente10@example.com', 'Aperta', 'Problema con l\'annuncio'),
+('cliente2@example.com', 'cliente10@example.com', 'Chiusa', 'Richiesta informazioni'),
+('cliente3@example.com', 'cliente10@example.com' ,'Aperta', 'Problema di pagamento'),
+('cliente4@example.com', 'cliente10@example.com', 'Chiusa', 'Richiesta di supporto'),
+('cliente5@example.com', 'cliente10@example.com', 'Aperta', 'Problema con l\'account');
+
+-- Popolamento della tabella Università
+INSERT INTO Università (denominazione, email, indirizzo) VALUES
+('Università di Roma', 'uniroma@example.com', 'Via Universitaria, 123'),
+('Politecnico di Milano', 'polimi@example.com', 'Via Politecnico, 456'),
+('Università di Napoli', 'unina@example.com', 'Via Napoletana, 789');
+
+-- Popolamento della tabella Iscrizione
+INSERT INTO Iscrizione (denominazione, email) VALUES
+('Università di Roma', 'cliente1@example.com'),
+('Politecnico di Milano', 'cliente2@example.com'),
+('Università di Napoli', 'cliente3@example.com'),
+('Politecnico di Milano', 'cliente4@example.com'),
+('Università di Napoli', 'cliente5@example.com'),
+('Università di Roma', 'cliente6@example.com'),
+('Politecnico di Milano', 'cliente7@example.com'),
+('Università di Napoli', 'cliente8@example.com'),
+('Università di Roma', 'cliente9@example.com'),
+('Politecnico di Milano', 'cliente10@example.com');
+
+-- Popolamento della tabella Chat
+INSERT INTO Chat (titolo) VALUES
+('Chat Viaggi'),
+('Chat Sport'),
+('Chat Cucina'),
+('Chat Musica'),
+('Chat Arte');
+
+-- Popolamento della tabella Partecipazione
+INSERT INTO Partecipazione (id_chat, email) VALUES
+(1, 'cliente1@example.com'),
+(1, 'cliente2@example.com'),
+(2, 'cliente3@example.com'),
+(2, 'cliente4@example.com'),
+(3, 'cliente5@example.com'),
+(3, 'cliente6@example.com'),
+(4, 'cliente7@example.com'),
+(4, 'cliente8@example.com'),
+(5, 'cliente9@example.com'),
+(5, 'cliente10@example.com');
+
+-- Popolamento della tabella Messaggio
+INSERT INTO Messaggio (email, id_chat, contenuto) VALUES
+('cliente1@example.com', 1, 'Ciao a tutti!'),
+('cliente2@example.com', 1, 'Salve! Dove avete viaggiato di recente?'),
+('cliente3@example.com', 2, 'Qualcuno vuole condividere ricette?'),
+('cliente4@example.com', 2, 'Ho appena corso una maratona, qualcuno vuole unirsi?'),
+('cliente5@example.com', 3, 'Qualcuno sa suonare uno strumento musicale?'),
+('cliente6@example.com', 3, 'Amo dipingere, chi condivide questa passione?'),
+('cliente7@example.com', 4, 'Consigli su libri d\'arte?'),
+('cliente8@example.com', 4, 'Qualcuno vuole fare una visita al museo?'),
+('cliente9@example.com', 5, 'Qualcuno vuole fare una passeggiata artistica?'),
+('cliente10@example.com', 5, 'Quali artisti preferite?');
+
+-- Popolamento della tabella Alloggio
+INSERT INTO Alloggio (tipo_alloggio, disponibilità, titolo, mq, n_camere_letto, n_bagni, classe_energetica, arredamenti, data_pubblicazione, pannelli_solari, pannelli_fotovoltaici, descrizione, verifica, prezzo, n_ospiti, n_stanze, tasse, email_dip, email_loc, data_verifica) VALUES
+('Camera', TRUE, 'Camera singola in centro', 20, 1, 1, 'A', TRUE, '2023-01-10', TRUE, FALSE, 'Bellissima camera in pieno centro della città', TRUE, 50.00, 1, 1, 10.00, 'dipendente1@example.com', 'cliente1@example.com', '2023-01-15'),
+('Monolocale', TRUE, 'Monolocale moderno', 40, 1, 1, 'B', TRUE, '2023-01-12', FALSE, TRUE, 'Monolocale con tutti i comfort', TRUE, 80.00, 2, 2, 15.00, 'dipendente1@example.com', 'cliente2@example.com', '2023-01-20'),
+('Stanza condivisa', TRUE, 'Stanza condivisa per studenti', 30, 2, 1, 'C', TRUE, '2023-02-01', FALSE, TRUE, 'Stanza condivisa in zona universitaria', TRUE, 40.00, 2, 2, 5.00, 'dipendente2@example.com', 'cliente3@example.com', '2023-02-10'),
+('Villa', TRUE, 'Villa con piscina', 200, 4, 3, 'A+', TRUE, '2023-02-15', TRUE, TRUE, 'Villa di lusso con vista panoramica', TRUE, 300.00, 6, 5, 50.00, 'dipendente2@example.com', 'cliente4@example.com', '2023-03-01'),
+('Bilocale', TRUE, 'Bilocale vicino al campus', 50, 1, 1, 'B', TRUE, '2023-03-01', FALSE, FALSE, 'Bilocale ideale per studenti', TRUE, 60.00, 2, 2, 10.00, 'dipendente1@example.com', 'cliente5@example.com', '2023-03-10');
+
+-- Popolamento della tabella Indirizzo
+INSERT INTO Indirizzo (id_alloggio, via, cap, civico, citta, provincia) VALUES
+(1, 'Via Principale', 12345, '12A', 'Roma', 'RM'),
+(2, 'Via Moderna', 56789, '34B', 'Milano', 'MI'),
+(3, 'Via Universitaria', 34567, '45', 'Roma', 'RM'),
+(4, 'Via Panoramica', 10123, '78', 'Napoli', 'NA'),
+(5, 'Via Campus', 54321, '56', 'Milano', 'MI');
+
+-- Popolamento della tabella Immagine
+INSERT INTO Immagine (id_alloggio, path) VALUES
+(1, '/images/camera.jpg'),
+(2, '/images/monolocale.jpg'),
+(3, '/images/stanza.jpg'),
+(4, '/images/villa.jpg'),
+(5, '/images/bilocale.jpg');
+
+-- Popolamento della tabella Affittare
+INSERT INTO Affittare (id_alloggio, email, data_inizio, data_fine, numero_carta, data_scadenza, prezzo) VALUES
+(1, 'cliente1@example.com', '2023-02-01', '2023-02-15', '1234567812345670', '2025-01-01', 400.00),
+(2, 'cliente2@example.com', '2023-02-10', '2023-02-25', '1234567812345670', '2025-01-01', 700.00),
+(3, 'cliente3@example.com', '2023-03-01', '2023-03-15', '1234567812345670', '2025-01-01', 300.00),
+(4, 'cliente4@example.com', '2023-03-15', '2023-03-30', '1234567812345670', '2025-01-01', 1000.00),
+(5, 'cliente5@example.com', '2023-04-01', '2023-04-15', '1234567812345670', '2025-01-01', 600.00);
+
+-- Popolamento della tabella Prenotazione
+INSERT INTO Prenotazione (id_alloggio, email, data_visita) VALUES
+(1, 'cliente2@example.com', '2023-01-25'),
+(2, 'cliente1@example.com', '2023-02-05'),
+(3, 'cliente4@example.com', '2023-02-20'),
+(4, 'cliente3@example.com', '2023-03-10'),
+(5, 'cliente6@example.com', '2023-04-05');
+
+-- Popolamento della tabella Recensione
+INSERT INTO Recensione (id_alloggio, email, titolo, voto, descrizione, data_recensione) VALUES
+(1, 'cliente1@example.com', 'Bellissima esperienza', 5, 'La camera era pulita e accogliente. Ottima posizione!', '2023-02-20'),
+(2, 'cliente2@example.com', 'Consigliato', 4, 'Il monolocale era spazioso e ben arredato. Buon rapporto qualità-prezzo.', '2023-03-01'),
+(3, 'cliente3@example.com', 'Esperienza positiva', 4, 'Stanza comoda e vicina all\'università. Proprietario gentile.', '2023-03-15'),
+(4, 'cliente4@example.com', 'Vacanza da sogno', 5, 'La villa è incredibile. Vista mozzafiato e tutti i comfort disponibili.', '2023-04-01'),
+(5, 'cliente5@example.com', 'Buon soggiorno', 3, 'Il bilocale era nella media. Niente di eccezionale ma sufficiente per brevi periodi.', '2023-04-10');
+
+-- Popolamento della tabella Servizi
+INSERT INTO Servizi (descrizione) VALUES
+('Wi-Fi'),
+('TV'),
+('Parcheggio'),
+('Giardino'),
+('Aria condizionata');
+
+-- Popolamento della tabella Possedimento
+INSERT INTO Possedimento (id_servizio, id_alloggio) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 4),
+(5, 5);
+
---
+++ WebSite/Database/URH DB.sql
@@ -0,0 +1,195 @@
+DROP SCHEMA unirenthub;
+CREATE  SCHEMA unirenthub;
+use unirenthub;
+
+
+-- Creazione della tabella Cliente
+CREATE TABLE Cliente (
+    email VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    tipo_utente ENUM('Studente', 'Locatore') NOT NULL,
+    data_nascita DATE,
+    numero_carta VARCHAR(16) CHECK(numero_carta >= 16),
+    data_scadenza DATE,
+    verificato BOOLEAN,
+    password VARCHAR(255) CHECK(LENGTH(password) >= 8) NOT NULL
+);
+
+CREATE TABLE Dipendente (
+    email VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    tipo_dipendente ENUM('Homechecker', 'Admin') NOT NULL,
+    password VARCHAR(255) CHECK(LENGTH(password) >= 8) NOT NULL
+);
+
+
+-- Creazione della tabella Post
+CREATE TABLE Post (
+    id_post INT AUTO_INCREMENT PRIMARY KEY,
+    titolo VARCHAR(255) NOT NULL,
+    descrizione LONGTEXT NOT NULL,
+    email VARCHAR(255),
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Passione
+CREATE TABLE Passione (
+    id_passione INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+-- Creazione della tabella Assegnazione
+CREATE TABLE Assegnazione (
+    id_passione INT,
+    email VARCHAR(255),
+    FOREIGN KEY (id_passione) REFERENCES Passione(id_passione) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Segnalazione
+CREATE TABLE Segnalazione (
+    email VARCHAR(255),
+    emailS VARCHAR(255),
+    stato VARCHAR(255) NOT NULL,
+    motivo VARCHAR(255) NOT NULL,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (emailS) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Università
+CREATE TABLE Università (
+    denominazione VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    indirizzo VARCHAR(255) NOT NULL
+);
+
+
+-- Creazione della tabella Iscrizione
+CREATE TABLE Iscrizione (
+    denominazione VARCHAR(255),
+    email VARCHAR(255),
+    FOREIGN KEY (denominazione) REFERENCES Università(denominazione) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Chat
+CREATE TABLE Chat (
+    id_chat INT AUTO_INCREMENT PRIMARY KEY,
+    titolo VARCHAR(255) NOT NULL
+);
+
+-- Creazione della tabella Partecipazione
+CREATE TABLE Partecipazione (
+    id_chat INT AUTO_INCREMENT,
+    email VARCHAR(255),
+    FOREIGN KEY (id_chat) REFERENCES Chat(id_chat) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Messaggio
+CREATE TABLE Messaggio (
+    id_messaggio INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
+    id_chat INT,
+    contenuto VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_chat) REFERENCES Chat(id_chat) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Alloggio
+CREATE TABLE Alloggio (
+    id_alloggio INT AUTO_INCREMENT,
+    tipo_alloggio ENUM('Stanza condivisa', 'Camera', 'Monolocale', 'Bilocale', 'Trilocale', 'Villa') NOT NULL,
+    disponibilità BOOLEAN NOT NULL,
+    titolo VARCHAR(255) NOT NULL,
+    mq INT CHECK(mq >= 1) NOT NULL,
+    n_camere_letto INT DEFAULT 1 CHECK(n_camere_letto > 0) NOT NULL,
+    n_bagni INT DEFAULT 1 CHECK(n_bagni > 0) NOT NULL,
+    classe_energetica ENUM('A++', 'A+', 'A', 'B', 'C', 'D') NOT NULL,
+    arredamenti BOOLEAN NOT NULL,
+    data_pubblicazione DATE NOT NULL,
+    pannelli_solari BOOLEAN NOT NULL,
+    pannelli_fotovoltaici BOOLEAN NOT NULL,
+    descrizione LONGTEXT NOT NULL,
+    verifica BOOLEAN NOT NULL,
+    prezzo DOUBLE CHECK(prezzo >= 0) NOT NULL,
+    n_ospiti INT DEFAULT 1 CHECK(n_ospiti > 0) NOT NULL,
+    n_stanze INT CHECK(n_stanze >= 1) NOT NULL,
+    tasse BOOLEAN NOT NULL,
+    email_dip VARCHAR(255),
+    email_loc VARCHAR(255),
+    data_verifica DATE,
+    PRIMARY KEY (id_alloggio),
+    FOREIGN KEY (email_dip) REFERENCES Dipendente(email) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (email_loc) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Indirizzo
+CREATE TABLE Indirizzo (
+    id_alloggio INT,
+    via VARCHAR(255) NOT NULL,
+    cap INT CHECK(cap >= 10000 AND cap <= 99999) NOT NULL,
+    civico VARCHAR(255) NOT NULL,
+    citta VARCHAR(255) NOT NULL,
+    provincia VARCHAR(2) CHECK(LENGTH(provincia) = 2) NOT NULL,
+    PRIMARY KEY (via, cap, civico, citta, provincia),
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Immagine
+CREATE TABLE Immagine (
+    id_immagine INT AUTO_INCREMENT PRIMARY KEY,
+    id_alloggio INT,
+    path VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Affittare
+CREATE TABLE Affittare (
+    id_alloggio INT,
+    email VARCHAR(255),
+    data_inizio DATE NOT NULL,
+    data_fine DATE NOT NULL,
+    numero_carta VARCHAR(16) CHECK(numero_carta >= 16) NOT NULL,
+    data_scadenza DATE NOT NULL,
+    prezzo DOUBLE DEFAULT 0 CHECK(prezzo >= 0) NOT NULL,
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Prenotazione
+CREATE TABLE Prenotazione (
+    id_alloggio INT,
+    email VARCHAR(255),
+    data_visita DATETIME NOT NULL,
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Recensione
+CREATE TABLE Recensione (
+    id_alloggio INT,
+    email VARCHAR(255),
+    titolo VARCHAR(255) NOT NULL,
+    voto INT CHECK(voto >= 0 AND voto <= 5) NOT NULL,
+    descrizione VARCHAR(255) NOT NULL,
+    data_recensione DATE NOT NULL,
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES Cliente(email) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Creazione della tabella Servizi
+CREATE TABLE Servizi (
+    id_servizio INT AUTO_INCREMENT PRIMARY KEY,
+    descrizione VARCHAR(255) NOT NULL
+);
+
+-- Creazione della tabella Possedimento
+CREATE TABLE Possedimento (
+    id_servizio INT,
+    id_alloggio INT,
+    FOREIGN KEY (id_alloggio) REFERENCES Alloggio(id_alloggio) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_servizio) REFERENCES Servizi(id_servizio) ON DELETE CASCADE ON UPDATE CASCADE
+);
