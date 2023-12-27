b2b7d233292e1a275b993f461a7aedb0cf02bd1a
AnthonyAlb02 <a.albanese19905.aa@gmail.com>
2023-12-27 08:21:16 +0100

    Front-end Homechecker

---
+++ WebSite/flask/static/js/jscleaner.js
@@ -0,0 +1,6 @@
+   window.onload = function() {
+        document.getElementById('email').value = '';
+        document.getElementById('nome').value = '';
+        document.getElementById('cognome').value = '';
+        document.getElementById('password').value = '';
+    }
